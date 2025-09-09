1abb0f59a595fe0ec0b5e9c60e056c1af5e8041a
ChunyiWang <83103931+chunyiw8080@users.noreply.github.com>
2025-09-09 18:21:20 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ cd /var/www/nodeapp || exit 1
 
 /root/.nvm/versions/node/v20.10.0/bin/npm install --production
 
-/root/.nvm/versions/node/v20.10.0/bin/pm2 restart server || /root/.nvm/versions/node/v20.10.0/bin/pm2 start app.js --name "nodeapp"
+/root/.nvm/versions/node/v20.10.0/bin/pm2 restart nodeapp || /root/.nvm/versions/node/v20.10.0/bin/pm2 start app.js --name "nodeapp"
 
 /root/.nvm/versions/node/v20.10.0/bin/pm2 ls
