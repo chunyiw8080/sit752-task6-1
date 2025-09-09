18a9285da70f4a30c2f95ece61ba78cf964b4ae8
ChunyiWang <83103931+chunyiw8080@users.noreply.github.com>
2025-09-09 18:19:12 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # deploy.sh
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 cd /var/www/nodeapp || exit 1
 
 /root/.nvm/versions/node/v20.10.0/bin/npm install --production
