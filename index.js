313ccb569555d36c38fecef4200663a6a6bac3f5
ChunyiWang <83103931+chunyiw8080@users.noreply.github.com>
2025-09-14 18:50:39 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const createServer = require('./server');
 
-const PORT = 3001;
+const PORT = 3002;
 const server = createServer();
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
